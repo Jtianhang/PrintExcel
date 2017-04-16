f83925244cad499d26c630c0e65911c747678e43
tianhang <916672393@qq.com>
2017-04-16 18:18:33 +0800

    tijiao
    Signed-off-by: tianhang <916672393@qq.com>

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -1,53 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.IO;
-using org.in2bits.MyXls;
-using System;
 using System.Collections.Generic;
+using UnityEngine.UI;
+
+
 public class test : MonoBehaviour
 {
-    TestInfo test1;
-    TestInfo test2;
-    TestInfo test3;
-    List<TestInfo> listInfos;
-    // Use this for initialization 
-    void Start()
-    {
-        // --测试数据  
-        test1 = new TestInfo();
-        test1.id = "one";
-        test1.name = "test1";
-        test1.num = "x";
-        test2 = new TestInfo();
-        test2.id = "two";
-        test2.name = "test2";
-        test2.num = "22";
-        test3 = new TestInfo();
-        test3.id = "tree";
-        test3.name = "test3";
-        test3.num = "333";
-        listInfos = new List<TestInfo>();
-        listInfos.Add(test1);
-        listInfos.Add(test2);
-        listInfos.Add(test3);
-        // --测试数据 
-    }
+    public Text FileNameText;
 
-    void OnGUI()
+    public void OnPrintExcel()
     {
-        if (GUI.Button(new Rect(100, 0, 100, 100), "aa"))
-        {
-            PrintExcel();
-            Debug.Log("aaaa");
-        }
-    }
-    public void PrintExcel()
-    {
-        if (!Directory.Exists(Application.dataPath + "/Prints"))
-        {
-            Directory.CreateDirectory(Application.dataPath + "/Prints");
-        }
-        string path = Application.dataPath + "/Prints/qiandao" + ".xls";
-        ExcelMakerManager.Instance.ExcelMaker(path, listInfos);
+        ExcelMakerManager.Instance.ExcelMaker(FileNameText.text);
     }
 }
